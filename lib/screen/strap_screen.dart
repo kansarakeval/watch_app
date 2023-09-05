eb1bcba8299fd34e362a0d253f47d1e8a7ca1792
os <kkeval304@gmail.com>
2023-09-05 17:58:46 +0530

    new

---
+++ lib/screen/strap_screen.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class StrapScreen extends StatefulWidget {
+  const StrapScreen({super.key});
+
+  @override
+  State<StrapScreen> createState() => _StrapScreenState();
+}
+
+class _StrapScreenState extends State<StrapScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
